8eaac14f9e6fdb18add46041e2824620a27fb395
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-08-31 13:13:06 +0200

    predeploy

--- src/pages/CategoriesList/CategoriesList.js
+++ src/pages/CategoriesList/CategoriesList.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './categoriesList.css';
 import Start from '../Start/Start';
 
