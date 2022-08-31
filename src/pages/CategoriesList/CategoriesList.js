2de7cb1c05ffba068c04a56486859c559fb3e410
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-08-31 14:32:25 +0200

    styled app

--- src/pages/CategoriesList/CategoriesList.js
+++ src/pages/CategoriesList/CategoriesList.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './categoriesList.css';
 import Start from '../Start/Start';
 
@@ -8,20 +8,28 @@ function CategoriesList() {
     return (
         <>
             <Router>
-               {/*  <nav>
+                {/* {<nav>
                     <ul>
                         <li><Link to='/quiz/movies'>Movies</Link></li>
                         <li><Link to='/quiz/dogs'>Dogs</Link></li>
                         <li><Link to='/quiz/flags'>Flags</Link></li>
-                        <li><Link to='/quiz/meals'>Dishes</Link></li>
+                        { <li><Link to='/quiz/meals'>Dishes</Link></li>}
                     </ul>
-                </nav> */}
+                </nav>
+                } */}
+
+                <div className='menu'>
+                    <button ><Link to='/quiz/dogs'>Dogs</Link></button>
+                    <button ><Link to='/quiz/flags'>Flags</Link></button>
+                    <button ><Link to='/quiz/movies'>Movies</Link></button>
+                </div>
 
                 <Routes>
                     <Route path='/quiz/:category' element={<Start />} />
                 </Routes>
 
             </Router>
+
         </>
     );
 }
